ce4e9f2ba66a147fdba4044d3e8c6c10fde751f4
Zhiwei Zhong <33756305+zhwzhong@users.noreply.github.com>
2024-01-31 11:37:44 -0300

    Update nir.py

--- data/nir.py
+++ data/nir.py
@@ -48,20 +48,14 @@ class NIR(Dataset):
 
         if self.attr == 'test':
             lr_img = gt_img / 255
+        
         else:
-            if self.args.mat_resize:
-                lr_img = imresize(gt_img.squeeze(), scalar_scale=1 / self.args.scale)
-            else:
-                lr_img = cv2.resize(gt_img.squeeze(), fx=1 / self.args.scale, fy=1 / self.args.scale, dsize=None)
+            lr_img = imresize(gt_img.squeeze(), scalar_scale=1 / self.args.scale)
+
             lr_img = imresize(lr_img.astype(float), scalar_scale=self.args.scale) / 255
             lr_img = np.expand_dims(lr_img, 0)
 
         gt_img, rgb_img = gt_img / 255, rgb_img / 255
-
-        if self.args.rgb_norm:
-            i_std = np.array([0.22773795, 0.22367531, 0.26343636]).reshape(3, 1, 1)
-            i_mean = np.array([0.45617331, 0.49676442, 0.44097971]).reshape(3, 1, 1)
-            rgb_img = (rgb_img - i_mean) / i_std
 
         lr_img, gt_img, rgb_img = augment.np_to_tensor(lr_img, gt_img, rgb_img, input_data_range=1)
 
