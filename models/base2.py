fcd10f4c71136b0bba216bec1cb7e5ba1f961ca1
Zhiwei Zhong <33756305+zhwzhong@users.noreply.github.com>
2024-01-31 11:40:38 -0300

    Update base2.py

--- models/base2.py
+++ models/base2.py
@@ -318,29 +318,3 @@ class Net(nn.Module):
 
 
 def make_model(args): return Net(args)
-
-# if __name__ == '__main__':
-#     img_channel = 3
-#     width = 32
-#
-#     # enc_blks = [2, 2, 4, 8]
-#     # middle_blk_num = 12
-#     # dec_blks = [2, 2, 2, 2]
-#
-#     enc_blks = [1, 1, 1, 28]
-#     middle_blk_num = 1
-#     dec_blks = [1, 1, 1, 1]
-#
-#     net = NAFNet(img_channel=img_channel, width=width, middle_blk_num=middle_blk_num,
-#                  enc_blk_nums=enc_blks, dec_blk_nums=dec_blks)
-#
-#     inp_shape = (3, 256, 256)
-#
-#     from ptflops import get_model_complexity_info
-#
-#     macs, params = get_model_complexity_info(net, inp_shape, verbose=False, print_per_layer_stat=False)
-#
-#     params = float(params[:-3])
-#     macs = float(macs[:-4])
-#
-#     print(macs, params)
